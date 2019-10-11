2dc12d459310a5ac89eb2bc06b497d956e5ea8a3
Jeanyusjoonyer <pinesjulie@googlemail.com>
2019-10-11 18:00:31 +0200

    added txt files

--- src/Converter.java
+++ src/Converter.java
@@ -10,16 +10,7 @@ public class Converter {
 
     public static void main(String[] args) {
 
-        /*
-        Map systemProperties = System.getProperties();
-
-        Set propertyKeys = systemProperties.keySet();
-
-        for(Object key : propertyKeys){
-            System.out.println(key + ": " + systemProperties.get(key));
-        }
-        */
-
+        //Create and Designate the Files
         crimeData = new File("crimeData.txt");
         convertedFile = new File("covertedFile.txt");
         myTextFile = new File("myTextFile.txt");
@@ -32,13 +23,64 @@ public class Converter {
                 e.printStackTrace();
             }
         }
+        try02();
+    }
 
-
+    public static void try02(){
         try (FileReader myFileReader = new FileReader(crimeData); BufferedReader myLineReader = new BufferedReader(myFileReader)){
 
             String line;
             int attributes = 0;
-            String [] var;
+            String [] tagName;
+            String [] TagValue;
+
+            //Set Mark to Recognize the Beginning of the Document
+            myLineReader.mark(1);
+            // Find out which attributes the File has and put them into a variable
+            for (String substring : myLineReader.readLine().split(",")) {
+                attributes++;
+                System.out.println(attributes);
+            }
+            // Go to the beginning of the document
+            myLineReader.reset();
+
+            ArrayList<String> list = new ArrayList<String>();
+            line = myLineReader.readLine();
+
+            System.out.println(line);
+
+            try (FileWriter myFileWriter = new FileWriter(convertedFile,false)){
+                myFileWriter.write(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void coverter (File input){
+        try (FileReader myFileReader = new FileReader(input); BufferedReader myLineReader = new BufferedReader(myFileReader)){
+            String line;
+            while ((line = myLineReader.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void try01 (){
+        try (FileReader myFileReader = new FileReader(crimeData); BufferedReader myLineReader = new BufferedReader(myFileReader)){
+
+            String line;
+            int attributes = 0;
+            String [] tagName;
+            String [] TagValue;
 
             //Set Mark to Recognize the Beginning of the Document
             myLineReader.mark(1);
@@ -53,22 +95,26 @@ public class Converter {
             myLineReader.reset();
 
 
-            var = new String [attributes];
-            var = myLineReader.readLine().split(",");
+            tagName = new String [attributes-1];
+            tagName = myLineReader.readLine().split(",");
 
-            for (int i = 0; i < var.length; i++){
-                System.out.println(var[i]);
+            for (int i = 0; i < tagName.length; i++){
+                System.out.println(tagName[i]);
             }
-
-
 
             try (FileWriter myFileWriter = new FileWriter(convertedFile, false) ){
 
-                for(int i = 0; i < var.length; i++) {
-                    myFileWriter.write(var[i] +"\n");
+                TagValue = new String [attributes-1];
+
+
+                while ((line = myLineReader.readLine()) != null) {
+                    TagValue = myLineReader.readLine().split(",");
+                    myFileWriter.write("crime ");
+                    for (int i = 0; i < tagName.length; i++) {
+                        myFileWriter.write(tagName[i] + "="  + TagValue[i] + " ");
+                    }
+                    myFileWriter.write("\n");
                 }
-
-
 
             }catch(IOException e){
                 e.printStackTrace();
@@ -77,21 +123,6 @@ public class Converter {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-    public static void coverter (File input){
-        try (FileReader myFileReader = new FileReader(input); BufferedReader myLineReader = new BufferedReader(myFileReader)){
-            String line;
-            while ((line = myLineReader.readLine()) != null){
-                System.out.println(line);
-            }
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
-        } catch (IOException e){
             e.printStackTrace();
         }
     }
